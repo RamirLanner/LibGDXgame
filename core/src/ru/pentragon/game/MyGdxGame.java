573c858d95b04a5450b7dd6aa44d15fb2cd74987
RamirLanner <pentragon@yandex.ru>
2022-04-18 18:44:09 +0300

    hw1

--- core/src/ru/pentragon/game/MyGdxGame.java
+++ core/src/ru/pentragon/game/MyGdxGame.java
@@ -1,6 +1,8 @@
 package ru.pentragon.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -8,6 +10,8 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	int posX=0;
+	int posY=0;
 	
 	@Override
 	public void create () {
@@ -19,7 +23,12 @@ public class MyGdxGame extends ApplicationAdapter {
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		if(Gdx.input.isButtonPressed(Input.Buttons.LEFT))
+		{
+			posX = Gdx.input.getX()-img.getWidth()/2;
+			posY = (Gdx.graphics.getHeight()-Gdx.input.getY()) - img.getHeight()/2;
+		}
+		batch.draw(img, posX, posY);
 		batch.end();
 	}
 	
