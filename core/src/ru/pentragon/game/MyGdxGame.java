7674a78c12d1428dee638c2a92466ab8954b2ef1
RamirLanner <pentragon@yandex.ru>
2022-04-18 18:50:26 +0300

    hw1

--- core/src/ru/pentragon/game/MyGdxGame.java
+++ core/src/ru/pentragon/game/MyGdxGame.java
@@ -21,15 +21,16 @@ public class MyGdxGame extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		if(Gdx.input.isButtonPressed(Input.Buttons.LEFT))
-		{
-			posX = Gdx.input.getX()-img.getWidth()/2;
-			posY = (Gdx.graphics.getHeight()-Gdx.input.getY()) - img.getHeight()/2;
-		}
-		batch.draw(img, posX, posY);
-		batch.end();
+		//test test
+			ScreenUtils.clear(1, 0, 0, 1);
+			batch.begin();
+			if(Gdx.input.isButtonPressed(Input.Buttons.LEFT))
+			{
+				posX = Gdx.input.getX()-img.getWidth()/2;
+				posY = (Gdx.graphics.getHeight()-Gdx.input.getY()) - img.getHeight()/2;
+			}
+			batch.draw(img, posX, posY);
+			batch.end();
 	}
 	
 	@Override
